bca417a55473c21d39a7bc586da1dd0937bf23d5
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-01-25 08:48:17 +0100

    form utllisable

--- src/Components/Pages/Contact/contact.jsx
+++ src/Components/Pages/Contact/contact.jsx
@@ -2,44 +2,96 @@ import Header from '../../Header/header'
 import RS from '../../RS/rs'
 import './contact.css'
 import { motion } from "framer-motion"
+import React, { useRef , useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { logDOM } from '@testing-library/react';
 
 function Contact(){
+  const form = useRef();
+  const FirstName = useRef();
+  const LastName = useRef();
+  const email = useRef();
+  const message = useRef();
+
+  const [modal, setModal] = useState(false);
+  const [errorModal, setErrorModal] = useState(false);
+  var erreurs = [];
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+      if(FirstName.current.value == ""){
+        erreurs.push('FirstName');
+        console.log(erreurs);
+      }
+      if(LastName.current.value == ""){
+        erreurs.push('LastName');
+        console.log(erreurs);
+      }
+      if(email.current.value == ""){
+        erreurs.push('email');
+      }
+      if(message.current.value == ""){
+        erreurs.push('message');
+      }
+      
+      if(erreurs.length == 0){
+        emailjs.sendForm('service_lgm7m6p', 'template_58bpf6r', form.current, 'FnMCT1Kk9TozDi78d')
+        setModal(!modal);
+
+      }
+      else{
+        setErrorModal(!errorModal);
+      }
+  };
+
+  const toogleModal = () => {
+    setModal(!modal);
+  }
+
+  const toogleErrorModal = () => {
+    setErrorModal(!errorModal);
+  }
+
+  {modal && 
+    setInterval(() => {
+        setModal(!modal);
+      }, 10000);
+  }
+
+  {errorModal && 
+    setInterval(() => {
+        setErrorModal(!errorModal);
+      }, 10000);
+  }
+
 
     return(
         <>
         <Header/>
         <main className='container-contact'>
             <motion.h1 initial={{y: -80, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.5}}>Contact me</motion.h1>
-            <form name="contact" method="post" data-netlify="true">
-            <div className='container-name-email'>
-            <input type="hidden" name="form-name" value="contact" />
-                <p>
-                    <input type="text" name="fisrtName" placeholder='First name'/>
-                </p>
-                <p>
-                    <input type="email" name="LastName" placeholder='Last name'/>
-                </p>
-                <p>
-                    <input type="email" name="email" placeholder='Email'/>
-                </p>
-          </div>
-          <p>
-            <textarea name="message" placeholder='message' className='message'></textarea>
-          </p>
-          <p>
-            <button type="submit" className='button-form'>Send</button>
-          </p>
-        </form>,
-            {/* <form action="">
+            <form ref={form} onSubmit={sendEmail}>
                 <div className='container-name-email'>
-                <motion.input initial={{y: -100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2}} type="text" name='FirstName' placeholder='First Name' />
-                <motion.input initial={{y: 100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.4}} type="text" name='LastName' placeholder='Last Name' />
-                <motion.input initial={{y: 200, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.6}} type="email" name='email' placeholder='Email' />
+                <motion.input ref={FirstName} initial={{y: -100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2}} type="text" name='FirstName' placeholder='First Name' />
+                <motion.input ref={LastName} initial={{y: 100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.4}} type="text" name='LastName' placeholder='Last Name' />
+                <motion.input ref={email} initial={{y: 200, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 0.6}} type="email" name='email' placeholder='Email' />
                 </div>
-                <motion.textarea initial={{y: -100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 1}} type="text" name='message' placeholder='Message' className='message' />
+                <motion.textarea ref={message} initial={{y: -100, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.2, delay: 1}} type="text" name='message' placeholder='Message' className='message' />
                 <motion.input initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 0.5, delay: 1.5}} type="submit" value="Send" className='button-form'/>
-            </form> */}
+            </form>
         </main>
+        {modal && (
+                <div className='modal-contact'>
+                    <h4>Votre Message à été envoyé avec succès !</h4>
+                    <p onClick={toogleModal}>close</p>
+                </div>
+            )}
+          {errorModal && (
+                <div className='modal-contact'>
+                    <h4>Il y a {erreurs.length} erreurs</h4>
+                    <p onClick={toogleErrorModal}>close</p>
+                </div>
+            )}
         <RS/>
         </>
     )
