cce915a697c0f650e3be9099980f8f8085bf80b4
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-09-11 16:52:40 +0200

    Update home.jsx

--- src/Components/Pages/Home/home.jsx
+++ src/Components/Pages/Home/home.jsx
@@ -15,7 +15,7 @@ function Home(){
         <main className={darkMode ? 'container-info_perso light' : 'container-info_perso'}>
         <motion.h1 initial={{y: -80, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.5}}>Evan Chauffour</motion.h1>
         <motion.h2 initial={{y: -20, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 0.5}}>Web developer</motion.h2>
-        <motion.a initial={{y: -20, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 1}} href="https://docdro.id/AMfcLFH" target="_blank">My CV</motion.a>
+        <motion.a initial={{y: -20, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 1}} href="https://docdro.id/13mSSeQ" target="_blank">My CV</motion.a>
         </main>
         <RS/>
         </>
